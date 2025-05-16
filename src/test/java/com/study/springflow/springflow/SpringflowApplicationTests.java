9b21be446ff2279c79092e1eafda2a1212de17d6
jun <pickjog@gmail.com>
2025-05-16 19:06:29 +0900

    first commit

---
+++ src/test/java/com/study/springflow/springflow/SpringflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springflow.springflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
